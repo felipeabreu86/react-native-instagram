f20748aa18e4d448891fb41558d122b7bb306ff9
felipeabreu <felipeabreu.rj@gmail.com>
2020-07-04 15:14:03 -0300

    Inclusao dos arquivos referentes ao projeto Instagrem para React Native"

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
-/** @format */
+import React from 'react'
+import { Provider } from 'react-redux'
+import { AppRegistry } from 'react-native'
+import App from './src/App'
+import {name as appName} from './app.json'
 
-import {AppRegistry} from 'react-native';
-import App from './App';
-import {name as appName} from './app.json';
+import storeConfig from './src/store/storeConfig'
 
-AppRegistry.registerComponent(appName, () => App);
+import axios from 'axios'
+axios.defaults.baseURL = 'https://lambe-bedee.firebaseio.com/'
+
+const store = storeConfig()
+const Redux = () => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+)
+
+AppRegistry.registerComponent(appName, () => Redux)
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,34 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import { Gravatar } from 'react-native-gravatar'
 import {
     StyleSheet,
     Text,
     View,
     Platform,
-    Image,
+    Image
 } from 'react-native'
 import icon from '../../assets/imgs/icon.png'
 
 class Header extends Component {
     render() {
+        const name = this.props.name || 'Anonymous'
+        const gravatar = this.props.email ?
+            <Gravatar options={{ email: this.props.email, secure: true }}
+                style={styles.avatar} />
+            : null
         return (
             <View style={styles.container}>
                 <View style={styles.rowContainer}>
                     <Image source={icon} style={styles.image} />
                     <Text style={styles.title}>Lambe Lambe</Text>
                 </View>
+                <View style={styles.userContainer}>
+                    <Text style={styles.user}>{name}</Text>
+                    {gravatar}
+                </View>
             </View>
-        );
+        )
     }
 }
 
@@ -44,8 +55,29 @@ const styles = StyleSheet.create({
         color: '#000',
         fontFamily: 'shelter',
         height: 30,
-        fontSize: 25
+        fontSize: 28
+    },
+    userContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+    },
+    user: {
+        fontSize: 10,
+        color: '#888',
+    },
+    avatar: {
+        width: 30,
+        height: 30,
+        marginLeft: 10,
     }
 })
 
-export default Header
+const mapStateToProps = ({ user }) => {
+    return {
+        email: user.email,
+        name: user.name,
+    }
+}
+
+// export default Header
+export default connect(mapStateToProps)(Header)
